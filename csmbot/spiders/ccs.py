960b19f2f3899b28313d12c454703eef55d6ee51
Kegan Maher <kegan.maher@smgov.net>
2014-01-17 15:19:01 -0800

    enforce spider contract for initial parse

--- csmbot/spiders/ccs.py
+++ csmbot/spiders/ccs.py
@@ -6,4 +6,12 @@ class CCSSpider(BaseSpider):
     start_urls = ["http://www.smgov.net/departments/ccs/content.aspx?id=32599"]
 
     def parse(self, response):
+        """
+	A spider contract:
+	http://doc.scrapy.org/en/latest/topics/contracts.html
+
+        @url http://www.smgov.net/departments/ccs/content.aspx?id=32599
+        @returns items 0 0
+        @returns requests 1 50
+ 	"""
         pass
