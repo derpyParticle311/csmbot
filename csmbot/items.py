b3e4d60a2e310bcb7eab480d43d955d92515651e
Kegan Maher <kegan.maher@smgov.net>
2014-01-17 14:50:55 -0800

    define items for Parks

--- csmbot/items.py
+++ csmbot/items.py
@@ -1,11 +1,16 @@
-# Define here the models for your scraped items
-#
 # See documentation in:
 # http://doc.scrapy.org/en/latest/topics/items.html
 
 from scrapy.item import Item, Field
 
-class CsmbotItem(Item):
-    # define the fields for your item here like:
-    # name = Field()
-    pass
+class Park(Item):
+    url = Field()
+    name = Field()
+    address = Field()
+    gmap = Field()
+    features = Field()
+    description = Field()
+
+class FeatureSet(Item):
+    name = Field()
+    features = Field()
