f49ded4f23debe9afb162e2a9b441fd5f818a939
Kegan Maher <kegan.maher@smgov.net>
2014-01-27 10:03:32 -0800

    moving some logic to item pipeline

--- csmbot/itemloaders.py
+++ csmbot/itemloaders.py
@@ -1,36 +1,9 @@
 import re
-from urlparse import urljoin
 
 from scrapy.contrib.loader import ItemLoader
-from scrapy.contrib.loader.processor import Identity, MapCompose, TakeFirst, Compose
+from scrapy.contrib.loader.processor import Identity, MapCompose, TakeFirst
 
 from csmbot.items import Park
-
-def shave(s):
-    return re.sub(r"((&nbsp;)|\s|\\xa0\<br/?>)+", " ", s).strip()
-
-def absurl(url):
-    return re.sub(r"^/", "http://www.smgov.net/", url)
-
-def getkids(s, loader_context):
-    match = re.match(r"<(h3|4)>(.+)</\1>", s)
-    if match:
-        featsname = match.group(2)
-        kids = loader_context.dicts["selector"].xpath("ul/li")
-    return s
-
-def getparts(s):
-    s1 = shave(s)
-    match = re.match(r"(.*?)\(?<a.*?href=(\"|')(.+?)\2.*?>(.+?)</a>\)?(.*?)", s1)
-    if match:
-        link = {"text": match.group(4), "url": absurl(match.group(3))}
-        ret = link
-        if match.group(1):
-            ret = match.group(1), ret
-        if match.group(5):
-            ret = ret, match.group(5)
-        return ret
-    return s1
 
 class ParkLoader(ItemLoader):
     default_item_class = Park
--- csmbot/pipelines.py
+++ csmbot/pipelines.py
@@ -1,8 +1,22 @@
-# Define your item pipelines here
-#
-# Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
 
-class CsmbotPipeline(object):
-    def process_item(self, item, spider):
-        return item
+import re
+
+def absurl(url):
+    return re.sub(r"^/", "http://www.smgov.net/", url)
+
+def shave(s):
+    return re.sub(r"((&nbsp;)|\s|\\xa0\<br/?>)+", " ", s).strip()
+	
+def getlinkparts(s):
+    s1 = shave(s)
+    match = re.match(r"(.*?)\(?<a.*?href=(\"|')(.+?)\2.*?>(.+?)</a>\)?(.*?)", s1)
+    if match:
+        link = {"text": match.group(4), "url": absurl(match.group(3))}
+        ret = link
+        if match.group(1):
+            ret = match.group(1), ret
+        if match.group(5):
+            ret = ret, match.group(5)
+        return ret
+    return s1
